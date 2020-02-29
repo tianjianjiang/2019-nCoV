e66e45df779b49e59c4df22bf0bce4439a99f5bb
Mike Tian-Jian Jiang <tmjiang@gmail.com>
2020-02-29 19:43:58 +0900

    Supply the missing `update_texts()`.

--- jquery_i18n/main-jquery_i18n.js
+++ jquery_i18n/main-jquery_i18n.js
@@ -2,6 +2,12 @@ $.i18n().load({
   en: 'i18n/en.json'
 })
 
+jQuery(document).ready(function() {
+  var update_texts = function() { $('body').i18n() };
+  $.i18n().load({...});
+  update_texts();
+});
+
 $('.lang-switch').click(function(e) {
   e.preventDefault();
   $.i18n().locale = $(this).data('locale');
